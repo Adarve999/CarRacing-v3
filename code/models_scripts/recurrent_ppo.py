4ee08004c394b06f6945da365c707cc20fb86b39
adarve <95427578+Adarve999@users.noreply.github.com>
2025-04-24 10:05:23 +0200

    empezando a migrar al nuevo juego

---
+++ code/models_scripts/recurrent_ppo.py
@@ -0,0 +1,47 @@
+from code.utils.utils import make_env
+import gymnasium as gym
+import ale_py
+from sb3_contrib import RecurrentPPO
+gym.register_envs(ale_py)
+import os
+from stable_baselines3.common.vec_env import SubprocVecEnv,VecMonitor
+
+def trainRecurrentPPO():
+    num_envs = 8
+
+    log_dir = "../logs/tensorboard/"
+    log_name = "rppo_carracing_lstm"
+    os.makedirs(log_dir, exist_ok=True)
+
+    envs = SubprocVecEnv([make_env(i) for i in range(num_envs)])
+    envs = VecMonitor(envs)
+
+    model = RecurrentPPO(
+            "CnnLstmPolicy",
+            envs,
+            device="cuda",
+            verbose=1,
+            tensorboard_log=log_dir,
+            n_steps=1024,
+            batch_size=64,
+            n_epochs=10,
+            gamma=0.99,
+            gae_lambda=0.95,
+            clip_range=0.2,
+            ent_coef=0.01,
+            max_grad_norm=0.5,
+            # parámetros específicos del LSTM
+            policy_kwargs=dict(
+                lstm_hidden_size=128,   # tamaño celdas (defecto)
+                n_lstm_layers=1,        # nº de capas LSTM
+                share_features_extractor=True,  # usa un solo extractor CNN
+            ),
+        )
+
+    model.learn(total_timesteps=1_000_000, tb_log_name=log_name)
+    model.save("../models/recurrent_ppo/recurrent_ppo_carracing")
+    envs.close()
+
+
+if __name__ == "__main__":
+    trainRecurrentPPO()
--- code/utils/utils.py
+++ code/utils/utils.py
@@ -1,45 +1,34 @@
 # Set up fake display; otherwise rendering will fail
 import gymnasium as gym
-import os
-from stable_baselines3.common.vec_env import VecVideoRecorder, DummyVecEnv
-
-
-def record_video(
-    env_id,
-    model,
-    video_length=500,
-    prefix="",
-    video_folder="../videos/",
-):
-    """
-    :param env_id: (str)
-    :param model: (RL model)
-    :param video_length: (int)
-    :param prefix: (str)
-    :param video_folder: (str)
-    """
-    eval_env = DummyVecEnv([lambda: gym.make(env_id, render_mode="rgb_array")])
-    # Start the video at step=0 and record 500 steps
-    eval_env = VecVideoRecorder(
-        eval_env,
-        video_folder=video_folder,
-        record_video_trigger=lambda step: step == 0,
-        video_length=video_length,
-        name_prefix=prefix,
-    )
-
-    obs = eval_env.reset()
-    for _ in range(video_length):
-        action, _ = model.predict(obs, deterministic=False)
-        obs, _, _, _ = eval_env.step(action)
-
-    # Close the video recorder
-    eval_env.close()
-    
+from gymnasium.wrappers import (RecordVideo)
 import base64
 from pathlib import Path
 
 from IPython import display as ipythondisplay
+
+def make_env():
+    def _init():
+        env = gym.make("CarRacing-v3", render_mode="rgb_array",continuous=False)
+        return env
+    return _init 
+
+
+def make_eval_env(modelIdentifier="model", max_steps=2000 ,record=False):
+    env = gym.make(
+        "CarRacing-v3",
+        render_mode="rgb_array",
+        continuous=False,
+        max_episode_steps=max_steps)
+
+    if record:
+        env = RecordVideo(
+            env,
+            video_folder="videos/",
+            name_prefix=f"{modelIdentifier}",
+            episode_trigger=lambda ep: True,
+            video_length=0
+        )
+    return env
 
 
 def show_videos(video_path="", prefix=""):
