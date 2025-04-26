5342edc5457a12c652db29871da035ff4176352c
javimp <202411137@alu.comillas.edu>
2025-04-26 15:07:10 +0200

    A2C

--- code/models_scripts/a2c.py
+++ code/models_scripts/a2c.py
@@ -1,8 +1,7 @@
 import os
 from utils.utils import make_env
 from stable_baselines3 import A2C
-from stable_baselines3.common.vec_env import SubprocVecEnv,VecMonitor
-
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecMonitor
 
 def trainA2C():
     num_envs = 8
@@ -11,7 +10,7 @@ def trainA2C():
     log_name = "a2c_carracing"
     os.makedirs(log_dir, exist_ok=True)
 
-    envs = SubprocVecEnv([make_env() for i in range(num_envs)])
+    envs = SubprocVecEnv([make_env() for _ in range(num_envs)])
     envs = VecMonitor(envs)
 
     model = A2C(
@@ -19,22 +18,18 @@ def trainA2C():
         envs,
         device="cuda",
         verbose=1,
-        n_steps=1024,
+        n_steps=128,          # Steps per environment per update
         gamma=0.99,
-        gae_lambda=1.0,
-        learning_rate=7e-4,
+        gae_lambda=0.95,
         ent_coef=0.01,
-        vf_coef=0.25, 
         max_grad_norm=0.5,
-        rms_prop_eps=1e-5,
+        learning_rate=1e-4,   # Slightly lower than default for stability
         tensorboard_log=log_dir
-        )
+    )
 
     model.learn(total_timesteps=1_000_000, tb_log_name=log_name)
     model.save("../models/a2c/a2c_carracing")
     envs.close()
 
-
 if __name__ == "__main__":
     trainA2C()
-
