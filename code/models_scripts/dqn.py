3911633aba96f8190f256b70a8eba1d33d23f22e
adarve <95427578+Adarve999@users.noreply.github.com>
2025-05-02 02:26:55 +0200

    modelo DQN terminado

--- code/models_scripts/dqn.py
+++ code/models_scripts/dqn.py
@@ -5,7 +5,7 @@ from stable_baselines3.common.vec_env import SubprocVecEnv,VecMonitor
 
 
 def trainDQN():
-    num_envs = 8
+    num_envs = 6
 
     log_dir = "../logs/tensorboard/"
     log_name = "dqn_carracing"
@@ -14,29 +14,25 @@ def trainDQN():
     envs = SubprocVecEnv([make_env() for i in range(num_envs)])
     envs = VecMonitor(envs)
 
-    # The best parameters obtained with the optuna model were:
-    # Parameters: {'learning_rate': 0.000571549193815661, 'gamma': 0.9932221896741693, 'exploration_fraction': 0.2603902541101231}. Best is trial 2 with value: 785.356467630182.
     model = DQN(
-        "CnnPolicy",
-        envs,
-        device="cuda",
-        verbose=1,
-        buffer_size=100_000,          
-        learning_starts=50_000,       
-        batch_size=32,                
-        train_freq=(4, "step"),                
-        target_update_interval=10_000,
-        tau=1.0,                      
-        gamma=0.9932221896741693,
-        learning_rate=0.000571549193815661,           
-        exploration_initial_eps=1.0,
-        exploration_final_eps=0.05,
-        exploration_fraction=0.2603902541101231,   
-        max_grad_norm=10,            
-        tensorboard_log=log_dir,
+        policy="CnnPolicy",
+        env=envs,
+        buffer_size        = 200_000,
+        learning_starts    = 20_000,
+        batch_size         = 64,
+        gamma              = 0.99,
+        target_update_interval = 10_000,
+        train_freq         = 4,            
+        gradient_steps     = 1,
+        exploration_fraction = 0.3,        
+        exploration_final_eps = 0.05,
+        policy_kwargs      = dict(net_arch=[256, 256]),
+        tensorboard_log    = log_dir,
+        device             = "cuda",
+        verbose            = 1,
     )
 
-    model.learn(total_timesteps=3_000_000, tb_log_name=log_name)
+    model.learn(total_timesteps=2_000_000, tb_log_name=log_name)
     model.save("../models/dqn/dqn_carracing")
     envs.close()
 
